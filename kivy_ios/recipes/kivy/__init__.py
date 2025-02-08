44d61f0a7d39a1d6f532abe8c695fe38d82ff900
kivyswiftlink <168654824+kivyswiftlink@users.noreply.github.com>
2025-02-08 13:34:17 +0100

    Update __init__.py

--- kivy_ios/recipes/kivy/__init__.py
+++ kivy_ios/recipes/kivy/__init__.py
@@ -8,8 +8,8 @@ logger = logging.getLogger(__name__)
 
 class KivyRecipe(CythonRecipe):
     version = "master"
-    #url = "https://github.com/kivy/kivy/archive/{version}.zip"
-    url = "https://github.com/kivyswiftlink/swifty-kivy/archive/{version}.zip"
+    url = "https://github.com/kivy/kivy/archive/{version}.zip"
+    #url = "https://github.com/kivyswiftlink/swifty-kivy/archive/{version}.zip"
     library = "libkivy.a"
     depends = [
                 "sdl2", "sdl2_image", "sdl2_mixer", "sdl2_ttf",
